96f0b3b060052f56bf378a2b85d4bf874fca5bff
aardvarkk <clarksonian@gmail.com>
2014-02-28 13:50:02 -0500

    Working with Selenium and less problems with it thinking we're a bot.

--- kayak_query.rb
+++ kayak_query.rb
@@ -3,8 +3,12 @@ require 'capybara-webkit'
 require 'capybara-user_agent'
 require 'open-uri'
 
-Capybara.current_driver = :webkit
+Capybara.current_driver = :selenium
+# Capybara.current_driver = :webkit
+# Capybara.javascript_driver = :webkit
 Capybara.default_wait_time = 5
+Capybara.ignore_hidden_elements = false
+
 # Capybara::UserAgent.add_user_agents googlebot: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 module Kayak
@@ -17,32 +21,57 @@ module Kayak
       # set_user_agent :googlebot
       # target = 'http://www.ca.kayak.com'
       # target = 'http://www.ca.kayak.com/flights'
-      # target = 'http://www.ca.kayak.com/flights?mc=y'
-      # visit target
+      target = 'http://www.ca.kayak.com/flights?mc=y'
+      visit target
 
-      # Fill in the form
-      # fill_in 'origin0', with: 'YYZ'
-      # fill_in 'destination0', with: 'ARN'
-      # fill_in 'depart_date0', with: '04/08/2014'
-      # fill_in 'origin1', with: 'ARN'
-      # fill_in 'destination1', with: 'YYZ'
-      # fill_in 'depart_date1', with: '18/08/2014'
-      # fill_in 'origin2', with: 'KIR'
-      # fill_in 'destination2', with: 'DUB'
-      # fill_in 'depart_date2', with: '25/08/2014'
-      # click_on 'fdimgbutton'
+      # Fill in the form (works in Selenium!)
+
+      fill_in 'origin0', with: 'YYZ'
+      # fill_in 'origincode0', with: 'YYZ/11592'
+      # find(:xpath, "//input[@id='origincode0']").set 'YYZ/11592'
+      Capybara.current_session.driver.execute_script("return document.getElementById('origincode0').value = 'YYZ/11592';")
+
+      fill_in 'destination0', with: 'ARN'
+      # fill_in 'destcode0', with: 'ARN/9880'
+      # find(:xpath, "//input[@id='destcode0']").set 'ARN/9880'
+      Capybara.current_session.driver.execute_script("return document.getElementById('destcode0').value = 'ARN/9880';")
+      fill_in 'depart_date0', with: '04/08/2014'
+
+      fill_in 'origin1', with: 'ARN'
+      # fill_in 'origincode1', with: 'ARN/9880'
+      # find(:xpath, "//input[@id='origincode1']").set 'ARN/9880'
+      Capybara.current_session.driver.execute_script("return document.getElementById('origincode1').value = 'ARN/9880';")
+      fill_in 'destination1', with: 'YYZ'
+      # fill_in 'destcode1', with: 'YYZ/11592'
+      # find(:xpath, "//input[@id='destcode1']").set 'YYZ/11592'
+      Capybara.current_session.driver.execute_script("return document.getElementById('destcode1').value = 'YYZ/11592';")
+      fill_in 'depart_date1', with: '18/08/2014'
+
+      fill_in 'origin2', with: 'KIR'
+      # fill_in 'origincode2', with: 'KIR/53064'
+      # find(:xpath, "//input[@id='origincode2']").set 'KIR/53064'
+      Capybara.current_session.driver.execute_script("return document.getElementById('origincode2').value = 'KIR/53064';")
+      fill_in 'destination2', with: 'DUB'
+      # fill_in 'destcode2', with: 'DUB/7362'
+      # find(:xpath, "//input[@id='destcode2']").set 'DUB/7362'
+      Capybara.current_session.driver.execute_script("return document.getElementById('destcode2').value = 'DUB/7362';")
+      fill_in 'depart_date2', with: '25/08/2014'
+
+      click_on 'fdimgbutton'
+
+      # sleep 10
 
       # print page.html
 
-      # save_screenshot 'screenshot.png'
+      save_screenshot 'screenshot.png'
 
       # response_headers.each { |h| puts h }
       # driver.cookies.each { |c| puts c }
       # driver.browser.get_cookies.each { |c| puts c }
 
-      target = 'http://www.ca.kayak.com/flights/YYZ-ARN/2014-08-04/ARN-YYZ/2014-08-18/KIR-DUB/2014-08-25'
-      visit target
-      save_screenshot 'screenshot.png'
+      # target = 'http://www.ca.kayak.com/flights/YYZ-ARN/2014-08-04/ARN-YYZ/2014-08-18/KIR-DUB/2014-08-25'
+      # visit target
+      # save_screenshot 'screenshot.png'
 
       # Capybara.current_session.driver.request.cookies.[]('auth_token').should_not be_nil
       # auth_token_value = Capybara.current_session.driver.request.cookies.[]('auth_token')
@@ -61,7 +90,7 @@ module Kayak
         return
       end
 
-      find '#progressDiv'
+      # find '#progressDiv'
       find '.pagecontrols'
       all('.bookitprice').each do |p|
         puts p.text
