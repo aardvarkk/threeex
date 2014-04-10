14b37d2d4c7a35762f5406d6853a72f5890e7ef7
aardvarkk <clarksonian@gmail.com>
2014-04-10 15:55:03 -0400

    Add support for splitting routes into separate maps. Uploaded "No YQ" generated images.

--- find_nonstop.rb
+++ find_nonstop.rb
@@ -9,25 +9,40 @@ opts = Trollop::options do
   opt :src, 'Source airport', type: :string
   opt :dst, 'Destination airport', type: :string
   opt :airline, 'Airline', type: :string
+  opt :map, 'Draw map'
+  opt :mapfile, 'Map filename', type: :string, default: 'map'
+  opt :groupsize, 'Map group size', default: 500
 end
 
 routes = File.open(PATH_PREFIX + 'routes.dat', 'rb').read
 
-matches = routes.scan /(#{opts[:airline].nil? ? '[\\w]{2}' : opts[:airline]}),[\d]+,(#{opts[:src]}),[\d]+,(#{opts[:dst].nil? ? '[\\w]{3}' : opts[:dst]})/
+matches = routes.scan /(#{opts[:airline].nil? ? '[\\w]{2}' : opts[:airline]}),[\d]+,(#{opts[:src].nil? ? '[\\w]{3}' : opts[:src]}),[\d]+,(#{opts[:dst].nil? ? '[\\w]{3}' : opts[:dst]})/
 matches.each { |m| puts "#{m[0]}-#{m[1]}-#{m[2]}"}
 
-# Create a map string from the matches
-map_str = []
-matches.each { |m| map_str << "#{m[1]}-#{m[2]}" }
-map_str = map_str.join ','
+# Split up into groups
+index = 0
+matches.each_slice opts[:groupsize] do |match_group|
 
-# Label all points
-# &PM=*
-RestClient.get "www.gcmap.com/map?P=#{map_str}&MS=wls2" do |response, request, result|
-    # p response
-    # p request
-    # p result
-    open('map.gif', 'wb') do |file|
-        file.write response.body
+  # Create a map string from the matches
+  map_str = []
+  match_group.each { |m| map_str << "#{m[1]}-#{m[2]}" }
+  map_str = map_str.join ','
+
+  # open('map_str', 'w') { |file| file.write map_str }
+
+  # Label all points
+  # &PM=*
+  if opts[:map]
+    RestClient.get "www.gcmap.com/map?P=#{map_str}&MS=wls2" do |response, request, result|
+        # p response
+        # p request
+        # p result
+        open("#{opts[:mapfile]}_#{index}.gif", 'wb') do |file|
+            file.write response.body
+        end
     end
+  end
+
+  index += 1
+
 end
